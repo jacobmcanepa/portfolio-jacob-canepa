9e0c244e6c537ad154e0283c1ed79ae4f683274c
Jacob Canepa <jacobmcanepa@gmail.com>
2022-11-15 20:04:20 -0800

    adjustments to mobile style

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,31 +1,35 @@
 import headshot from '../../assets/images/jacob-headshot.jpg';
-import { Container } from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faLinkedinIn, faGithub, faInstagram } from '@fortawesome/free-brands-svg-icons';
 
 function Home() {
   return (
-    <Container>
-      <div className='text-center'>
-        <img src={headshot} alt='jacob-headshot' className='headshot img-fluid'/>
-        <h2 className='mt-4 profession'>Full-stack Developer</h2>
-        <p className='mx-2 pt-3'>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec bibendum turpis sed ex
-          condimentum molestie. Mauris condimentum lectus ut ornare dignissim. Mauris faucibus urna
-          mi, ac feugiat metus aliquam maximus.
-        </p>
-      </div>
-      <div className='text-center social-icon'>
-        <a href='https://www.linkedin.com/in/jacob-canepa-568740198/' target='_blank' rel='noreferrer'>
-          <FontAwesomeIcon icon={faLinkedinIn} />
-        </a>
-        <a href='https://github.com/jacobmcanepa' target='_blank' rel='noreferrer'>
-          <FontAwesomeIcon icon={faGithub} />
-        </a>
-        <a href='https://instagram.com' target='_blank' rel='noreferrer'>
-          <FontAwesomeIcon icon={faInstagram} />
-        </a>
-      </div>
+    <Container className='text-center'>
+      <Row>
+        <Col md={12} lg={4}>
+          <img src={headshot} alt='jacob-headshot' className='headshot img-fluid'/>
+        </Col>
+        <Col md={12} lg={4}>
+          <h2 className='mt-3 profession'>Full - stack Developer</h2>
+          <p className='mt-4'>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec bibendum turpis sed ex
+            condimentum molestie. Mauris condimentum lectus ut ornare dignissim. Mauris faucibus urna
+            mi, ac feugiat metus aliquam maximus.
+          </p>
+        </Col>
+        <Col className='social-icon mt-4' md={12} lg={4}>
+          <a href='https://www.linkedin.com/in/jacob-canepa-568740198/' target='_blank' rel='noreferrer'>
+            <FontAwesomeIcon icon={faLinkedinIn} />
+          </a>
+          <a href='https://github.com/jacobmcanepa' target='_blank' rel='noreferrer'>
+            <FontAwesomeIcon icon={faGithub} />
+          </a>
+          <a href='https://instagram.com' target='_blank' rel='noreferrer'>
+            <FontAwesomeIcon icon={faInstagram} />
+          </a>
+        </Col>
+      </Row>
     </Container>
   );
 };
