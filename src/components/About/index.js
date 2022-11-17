639ab07e85b1fe74c66fe93af581c13f4dc2e17c
Jacob Canepa <jacobmcanepa@gmail.com>
2022-11-17 03:05:07 -0800

    about section rework

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,29 +1,26 @@
 function About() {
   return (
     <section className="about-section">
-      <div className="about-container">
-        <div className="about-card">
-          <h2>About</h2>
-          <p className="mt-5">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec bibendum turpis sed ex
-            condimentum molestie. Mauris condimentum lectus ut ornare dignissim. Mauris faucibus urna
-            mi, ac feugiat metus aliquam maximus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec bibendum turpis sed ex
-            condimentum molestie.
-            <br />
-            Lorem ipsum dolar sit amet, consectetur adipiscing elit. Donec bibendum. turpis sed ex condimentum molestie Lorem ipsum.
-          </p>
-        </div>
-        <div className="skills-card">
-          <h2>Skills</h2>
-          <ul className="mt-5">
-            <li>HTML</li>
-            <li>CSS</li>
-            <li>Javascript</li>
-            <li>MERN Stack</li>
-            <li>Progressive Web Applications</li>
+      <article>
+        <h2>About</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec bibendum turpis sed ex
+          condimentum molestie. Mauris condimentum lectus ut ornare dignissim. Mauris faucibus urna
+          mi, ac feugiat metus aliquam maximus.
+          <br />
+          Condimentum molestie. dolor sit amet, consectetur adipiscing elit. ac feugiat metus aliquam maximus.
+        </p>
+
+        <div className="skills-list">
+          <h3>Skills</h3>
+          <ul>
+            <li>HTML, CSS, Javascript</li>
+            <li>MongoDB, Express.js, React, Node.js</li>
+            <li>Sequelize, Mongoose, Bootstrap</li>
+            <li>Git, MySQL</li>
           </ul>
         </div>
-      </div>
+      </article>
     </section>
   );
 }
