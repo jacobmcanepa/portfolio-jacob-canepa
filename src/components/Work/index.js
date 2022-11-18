da9f2b1d7b7bd996f1b6d4bb697bbbf167f90dae
Jacob Canepa <jacobmcanepa@gmail.com>
2022-11-18 00:36:24 -0800

    work page outlined

--- src/components/Work/index.js
+++ src/components/Work/index.js
@@ -1,6 +1,42 @@
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 function Work() {
   return (
-    <h2>Work Page</h2>
+    <section className='work-section'>
+      <Container fluid='xxl' className="work-container">
+        <Row className='work-row'>
+          <Col sm={12} lg={4}>
+            <div className='work-title'>
+              <h2>Projects</h2>
+            </div>
+          </Col>
+          <Col sm={12} lg={8}>
+            <div className='main-project'>
+              Main Project
+            </div>
+          </Col>
+        </Row>
+        <Row>
+          <Col sm={12} lg={4}>
+            <div className='sub-project'>
+              Project 1
+            </div>
+          </Col>
+          <Col sm={12} lg={4}>
+            <div className='sub-project'>
+              Project 2
+            </div>
+          </Col>
+          <Col sm={12} lg={4}>
+            <div className='sub-project'>
+              Project 3
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </section>
   );
 }
 
