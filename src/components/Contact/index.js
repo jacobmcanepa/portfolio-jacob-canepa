aea9fedef11126d3dd33b432b3cc20505dbe3687
Jacob Canepa <jacobmcanepa@gmail.com>
2022-11-18 01:40:05 -0800

    contact page rough draft

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,6 +1,19 @@
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 function Contact() {
   return (
-    <h2>Contact Page</h2>
+    <section className='contact-section'>
+      <Container fluid='lg' className='contact-container'>
+        <Row>
+          <Col className='contact-question'>Like what you see?</Col>
+        </Row>
+        <Row>
+          <Col className='contact-me'><a href='mailto:jacobmcanepa@gmail.com'>Contact Me</a></Col>
+        </Row>
+      </Container>
+    </section>
   );
 }
 
