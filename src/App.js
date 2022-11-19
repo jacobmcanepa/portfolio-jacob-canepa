3b44743e09e89b925c77d4a23d23d2b8f0ab8e7c
Jacob Canepa <jacobmcanepa@gmail.com>
2022-11-19 01:48:15 -0800

    switched navigation from plain js to react router dom

--- src/App.js
+++ src/App.js
@@ -1,56 +1,26 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navigation from "./components/Nav";
 import Home from "./components/Home";
 import About from "./components/About";
 import Work from "./components/Work";
 import Contact from "./components/Contact";
-import Footer from "./components/Footer";
+// import Footer from "./components/Footer";
 
 function App() {
-  let component;
-  let isFooter = true;
-  switch (window.location.pathname) {
-    case "/":
-      component = <Home />
-      break
-    case "/home":
-      component = <Home />
-      break
-    case "/about":
-      component = <About />
-      document.title = "About"
-      break
-    case "/work":
-      component = <Work />
-      document.title = "Work"
-      isFooter = false;
-      break
-    case "/contact":
-      component = <Contact />
-      document.title = "Contact"
-      break
-    default:
-  }
-
-  if (isFooter) {
-    return (
-      <>
+  return (
+    <>
+    <BrowserRouter>
       <Navigation />
-      <main>
-        {component}
-      </main>
-      <Footer />
-      </>
-    );
-  } else {
-    return (
-      <>
-      <Navigation />
-      <main>
-        {component}
-      </main>
-      </>
-    );
-  }
-}
+      <Routes>
+        <Route path='/' element={ <Home /> } />
+        <Route path='home' element={ <Home /> } />
+        <Route path='about' element={ <About /> } />
+        <Route path='work' element={ <Work /> } />
+        <Route path='contact' element={ <Contact /> } />
+      </Routes>
+    </BrowserRouter>
+    </>
+  );
+};
 
 export default App;
